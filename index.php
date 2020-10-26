c387eaedc94a95824374726fe88c1fea87c0e399
webew <fillion.bertrand@gmail.com>
2020-10-26 22:00:46 +0100

    fin

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
 
+// Connexion à la base de données
 $conn = new PDO("mysql:host=localhost;dbname=dwwm_2020;charset=utf8mb4", "root", "", []);
 
+// Requête SQL + envoi
 $stmt = $conn->prepare("SELECT * FROM etudiant");
-
 $stmt->execute();
 
+// Récupération de tous les enregistrements sous forme de tableau associatif
 $etudiants = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 var_dump($etudiants);
 
-// $villes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-// foreach($villes as $ville){
-//     var_dump($ville);
+// Récupération des enregistrements individuellement
+// while ($etudiant = $stmt->fetch()) {
+//     var_dump($etudiant["nom"]);
 // }
-
-while ($etudiant = $stmt->fetch()) {
-    var_dump($etudiant["nom"]);
-}
--- insert.php
+++ insert.php
@@ -1,14 +1,16 @@
 <?php
-var_dump($_POST);
-
+// Les données en provenance du formulaire
 $nom = $_POST["nom"];
 $prenom = $_POST["prenom"];
 
+// Connexion à la base de données
 $conn = new PDO("mysql:host=localhost;dbname=dwwm_2020;charset=utf8mb4", "root", "", []);
 
+// Requête SQL
 $sql = "INSERT INTO etudiant(nom,prenom) VALUES (:nom, :prenom)";
+
+// Envoi de la requête SQL à la base de données
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':nom', $nom);
 $stmt->bindParam(':prenom', $prenom);
-
 $stmt->execute();
